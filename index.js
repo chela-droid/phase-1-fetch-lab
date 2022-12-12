7f47dfe07efcf78516211911ccb5fd959168cfd8
chela-droid <[200~chelangatcaroline70@gmail.com~>
2022-12-12 07:45:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 function fetchBooks() {
+
   // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(function(response){
+    return response.json ();
+
+  })
+  .then(function(data){
+    renderBooks(data)
+  })
+  .catch(function(error){
+    console.log(error)
+  })
+
+
+
+
+  // .then((json) => console.log(json));
   
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
+// function renderBooks(books) {
+  // const main = document.querySelector('main');
+  // books.forEach(book => {
+    // const h2 = document.createElement('h2');
+    // h2.innerHTML = book.name;
+    // main.appendChild(h2);
+  // });
+// }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+// document.addEventListener('DOMContentLoaded', function() {
+  // fetchBooks();
+// });
+
+//  function renderBooks(){
+
+//  }
+//renders book titles into the DOM by passing a JSON object to renderBooks
+
+
+  // document.addEventListener('DOMContentLoaded',function(){
+  //   fetchBooks();
+
+  // });
+ 
+
+
